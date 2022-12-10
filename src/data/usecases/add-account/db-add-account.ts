948c9446ed096d980d0d471d50b45dac9cb8a0ca
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2022-12-10 14:27:20 -0300

    feat: ensure AddAccount

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,10 +1,20 @@
-import { AddAccount, AccountModel, Encrypter, AddAccountModel } from './db-add-account-protocols'
+import {
+  AddAccount,
+  AccountModel,
+  Encrypter,
+  AddAccountModel,
+  AddAccountRepository
+} from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
-  constructor (private readonly encrypter: Encrypter) { }
+  constructor (
+    private readonly encrypter: Encrypter,
+    private readonly addAccountRepository: AddAccountRepository
+  ) { }
 
-  async add (account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password)
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password)
+    await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
     return {
       password: 'aaa',
       email: 'aa',

631286309ce7db5a002bf156aad1409701b45ded
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2022-12-11 11:35:37 -0300

    refactor: move mongodb methods to a helper

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -4,7 +4,8 @@ module.exports = {
       version: '4.0.3',
       skipMD5: true
     },
-    autoStart: false,
-    instance: {}
-  }
+    instance: {},
+    autoStart: false
+  },
+  mongoURLEnvName: 'MONGODB_URI'
 }

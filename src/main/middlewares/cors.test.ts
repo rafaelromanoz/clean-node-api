ab3ba3e9d9269c5c55f367cf730c022285854186
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2022-12-11 13:00:18 -0300

    feat: ensure cors is enabled

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,13 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Cors Middleware', () => {
+  test('Should enabled cors', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .get('/test_cors')
+      .expect('acess-control-allow-origin', '*')
+  })
+})

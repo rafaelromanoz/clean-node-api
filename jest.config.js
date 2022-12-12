d06b606ed9711f2ea34ea21413fc251aed8c8c5f
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2022-12-12 10:28:40 -0300

    feat: ensure mongodb is connected

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,13 +3,16 @@ import { AccountModel } from '../../../../domain/models/account'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
+  uri: null as unknown as string,
   async connect (uri: string): Promise<void> {
+    this.uri = uri
     this.client = await MongoClient.connect(uri)
   },
   async disconnect () {
     await this.client.close()
   },
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
+    await this.connect(this.uri)
     return this.client.db().collection(name)
   },
   map (accountData: any, accountId: string): AccountModel {

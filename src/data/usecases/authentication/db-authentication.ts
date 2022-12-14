d05ca9b0491fcdf1343fe9bd807e30d3270f321c
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2022-12-14 10:54:08 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,5 +1,6 @@
 import { HashCompare } from 'data/protocols/criptography/hash-comparer'
 import { TokenGenerator } from 'data/protocols/criptography/token-generator'
+import { UpdateAccessTokenRepository } from 'data/protocols/db/update-access-token-repository'
 import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication/authentication'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
@@ -7,14 +8,19 @@ export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashCompare: HashCompare,
-    private readonly tokenGenerator: TokenGenerator
+    private readonly tokenGenerator: TokenGenerator,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) { }
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
     const account = await this.loadAccountByEmailRepository.load(authentication.email)
     if (account) {
       const isValid = await this.hashCompare.compare(authentication.password, account.password)
-      if (isValid) return this.tokenGenerator.generate(account.id)
+      if (isValid) {
+        const acessToken = await this.tokenGenerator.generate(account.id)
+        await this.updateAccessTokenRepository.update(account.id, acessToken)
+        return acessToken
+      }
     }
     return null
   }

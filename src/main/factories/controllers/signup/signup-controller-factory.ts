01fae055178667cb9a058f29224c09da0e203072
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2023-01-04 10:34:00 -0300

    feat: ensure AddSurveyController returns 500 if addsurvey usecase throws

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,6 +1,6 @@
 import { makeDbAddAccount } from './../../usecases/add-account/db-add-account-factory'
 import { makeDbAuthentication } from './../../usecases/authentication/db-authentication-factory'
-import { SignUpController } from '../../../../presentation/controllers/login2/signup/signup-controller'
+import { SignUpController } from '../../../../presentation/controllers/login/signup/signup-controller'
 import { Controller } from '../../../../presentation/protocols'
 import { makeSignUpValidation } from './signup-validation-factory'
 import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -0,0 +1,32 @@
+import { serverError } from './../../../helpers/http/http-helper'
+import { badRequest } from '../../../helpers/http/http-helper'
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  Validation,
+  AddSurvey
+} from './add-survey-controller-protocols'
+
+export class AddSurveyController implements Controller {
+  constructor (
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
+  ) { }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse | undefined> {
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const { question, answers } = httpRequest.body
+      await this.addSurvey.add({
+        question,
+        answers
+      })
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}

683e1879c8c0c7c23cfb682661a81b2d7b61eb36
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2023-01-06 10:21:37 -0300

    test: ensure add survey route returns 204 if valida access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,3 +1,4 @@
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
@@ -5,6 +6,7 @@ import app from '../config/app'
 import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -17,6 +19,8 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
   describe('POST /surveys', () => {
     test('Should return 403 on addsurvey without accessToken', async () => {
@@ -34,6 +38,39 @@ describe('Survey Routes', () => {
           }]
         })
         .expect(403)
+    })
+
+    test('Should return 204 with valid token', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Rafael',
+        email: 'rafael@gmail.com',
+        password: '123',
+        role: 'admin'
+      })
+      const id = res.insertedId?.toHexString()
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: res.insertedId
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          },
+          {
+            answer: 'Answer 2',
+            image: 'http://image-name.com'
+          }]
+        })
+        .expect(204)
     })
   })
 })

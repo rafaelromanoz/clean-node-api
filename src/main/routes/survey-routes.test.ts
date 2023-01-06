3f2d6679130b8e1d409443ff7dd05c7f183b5960
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2023-01-06 19:53:42 -0300

    test: ensure load surveys route returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -73,4 +73,44 @@ describe('Survey Routes', () => {
         .expect(204)
     })
   })
+  describe('GET /surveys', () => {
+    test('Should return 403 on load surveys without accessToken', async () => {
+      await request(app)
+        .get('/api/surveys')
+        .expect(403)
+    })
+
+    test('Should return 204 with valid token', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Rafael',
+        email: 'rafael@gmail.com',
+        password: '123',
+        role: 'admin'
+      })
+      const id = res.insertedId?.toHexString()
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: res.insertedId
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          },
+          {
+            answer: 'Answer 2',
+            image: 'http://image-name.com'
+          }]
+        })
+        .expect(204)
+    })
+  })
 })

e1e94826f4b1a8082c258510405ca1f248274f92
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2023-01-06 15:52:22 -0300

    feat: ensure LoadSurveysController retuens 200 on success

--- src/presentation/controllers/survey/load-survey/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-survey/load-surveys-controller.ts
@@ -1,13 +1,11 @@
+import { ok } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadSurveys } from './load-surveys-controller-protocols'
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) { }
 
   async handle (httpResquest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveys.load()
-    return {
-      body: '',
-      statusCode: 200
-    }
+    const surveys = await this.loadSurveys.load()
+    return ok(surveys)
   }
 }

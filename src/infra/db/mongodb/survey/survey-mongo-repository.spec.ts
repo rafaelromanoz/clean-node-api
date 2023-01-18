b756ba29beb98d4e60be9daea661d1bff994163e
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2023-01-18 10:48:31 -0300

    test: ensure SurveyMongoRepository find survey by id with success

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -40,6 +40,7 @@ describe('Survey Mongo Repository', () => {
       expect(survey).toBeTruthy()
     })
   })
+
   describe('loadAll()', () => {
     test('Should load all surveys on sucess', async () => {
       const sut = makeSut()
@@ -69,6 +70,32 @@ describe('Survey Mongo Repository', () => {
       expect(surveys.length).toBe(2)
       expect(surveys[0].question).toBe('any_question')
       expect(surveys[1].question).toBe('other_question')
+    })
+
+    test('Should load empty list', async () => {
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(0)
+    })
+  })
+
+  describe('loadById()', () => {
+    test('Should load survey by id on success', async () => {
+      const sut = makeSut()
+      const res = await surveyCollection.insertOne({
+        question: 'any_question',
+        date: new Date(),
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+        {
+          answer: 'other_answer'
+        }]
+      })
+      const id = res.insertedId.toHexString()
+      const survey = await sut.loadById(id)
+      expect(survey).toBeTruthy()
     })
 
     test('Should load empty list', async () => {

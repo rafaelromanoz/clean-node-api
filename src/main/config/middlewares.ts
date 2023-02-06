eae538b080bd44320feb9032544ee536a70e575d
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2023-02-06 18:48:13 -0300

    feat: add no cache middleware

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,9 @@
 import { Express } from 'express'
 import { bodyParser, contentType, cors } from '../middlewares'
+import setupSwagger from './config-swagger'
 
 export default (app: Express): void => {
+  setupSwagger(app)
   app.use(bodyParser)
   app.use(cors)
   app.use(contentType)
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openApi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'API clean node para realizar enquetes entre programadores',
+    version: '1.0.0'
+  }
+}

9deb6ba1acf4df4dccdfcf557b6f00531cd2e9f3
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2023-02-06 18:58:38 -0300

    feat: apply nocache middleware on swagger api

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,8 @@
 export default {
-  openApi: '3.0.0',
+  openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
-    description: 'API clean node para realizar enquetes entre programadores',
+    description: 'API clean node para realizar enquetes',
     version: '1.0.0'
   }
 }
